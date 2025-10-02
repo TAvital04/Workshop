ccc49fd33252fc0fe12e2cdd1f811c986152c6bc
Tal Avital <tal.avital04@gmail.com>
2025-10-02 15:05:12 -0400

    query to qdb

--- quest.py
+++ quest.py
@@ -1,9 +1,23 @@
 from questdb.ingress import Sender
+from datetime import datetime
 import sine
 
-conf = ""
+conf = 'http::addr=localhost:9000;'
+table_name = 'telemetry-data'
 
 with Sender.from_conf(conf) as sender:
     while True:
         y = sine.getY()
+
+        sender.row(
+            table_name,
+            columns = {
+                'yValues': y
+            },
+            at = datetime.now()
+        )
+
+        print(y)
+
+        sender.flush()
         
--- sine.py
+++ sine.py
@@ -3,6 +3,9 @@ import math
 x = 0
 
 def getY():
+    global x
+
     result = math.sin(x)
+    
     x += .001
     return result
